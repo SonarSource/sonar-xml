bc10ae34354a6b24f909c9e7fc1e60fd3dc42546
Dinesh Bolkensteyn <dinesh.bolkensteyn@sonarsource.com>
2013-02-04 14:27:40 +0100

    Upgrade to a multimodule project, remove some useless files (in cmd directory) and fix some EOLs

--- cmd/validate-sonar-maven.bat
+++
@@ -1,8 +0,0 @@
-rem run this script from xml folder
-rem the script starts analysis of maven files in the csharp folder
-
-set DEBUG=-X
-
-set mvncommand=mvn sonar:sonar -Dmaven.test.skip=true -Dsonar.language=xml -Dsonar.xml.sourceDirectory=. -Dsonar.xml.includeFileFilter=**/pom.xml -Dsonar.xml.schemas=http://maven.apache.org/POM/4.0.0
- 
-call %mvncommand% -N -f ../pom.xml %SONAR_FLAGS% %DEBUG% 
