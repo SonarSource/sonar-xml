89b6860e45063ea91703c691d51e1f43673a00d1
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2015-11-16 11:56:41 +0100

    Add IT for highlighting with BOM

---
+++ its/plugin/src/test/java/com/sonar/it/xml/ByteOrderMarkTest.java
@@ -0,0 +1,57 @@
+/*
+ * XML :: IT
+ * Copyright (C) 2013 ${owner}
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package com.sonar.it.xml;
+
+import com.google.common.collect.ImmutableMap;
+import com.sonar.orchestrator.Orchestrator;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.sonar.wsclient.SonarClient;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.File;
+
+public class ByteOrderMarkTest {
+
+  private static final String PROJECT_KEY = "byte-order-mark";
+
+  @ClassRule
+  public static Orchestrator orchestrator = XmlTestSuite.ORCHESTRATOR;
+
+  @Test
+  public void test() throws Exception {
+    orchestrator.getServer().provisionProject(PROJECT_KEY, PROJECT_KEY);
+    orchestrator.getServer().associateProjectToQualityProfile(PROJECT_KEY, "xml", "it-profile");
+    orchestrator.executeBuild(XmlTestSuite.createSonarRunner()
+      .setProjectDir(new File("projects/" + PROJECT_KEY))
+      .setProjectKey(PROJECT_KEY)
+      .setProjectName(PROJECT_KEY)
+      .setProjectVersion("1.0")
+      .setSourceEncoding("utf-8")
+      .setSourceDirs("."));
+
+    SonarClient sonarClient = orchestrator.getServer().adminWsClient();
+    Object fileKey = PROJECT_KEY + ":utf8-bom.xml";
+    String highlighting = sonarClient.get("/api/sources/show", ImmutableMap.of("key", fileKey));
+    assertThat(highlighting).contains("<span class=\\\"k\\\">&lt;?xml</span>");
+  }
+
+}
