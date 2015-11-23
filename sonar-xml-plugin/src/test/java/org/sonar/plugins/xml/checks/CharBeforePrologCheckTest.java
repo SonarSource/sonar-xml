6e077f01db8b01071b57f446a8fbdee0149be50b
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2015-11-23 13:46:39 +0100

    Fix unit test on windows

--- sonar-xml-plugin/src/test/java/org/sonar/plugins/xml/checks/CharBeforePrologCheckTest.java
+++ sonar-xml-plugin/src/test/java/org/sonar/plugins/xml/checks/CharBeforePrologCheckTest.java
@@ -20,8 +20,9 @@ package org.sonar.plugins.xml.checks;
 import static org.junit.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
-
+import java.nio.charset.StandardCharsets;
 import org.junit.Test;
+import org.sonar.api.batch.fs.internal.DefaultFileSystem;
 
 /**
  * @author Matthijs Galesloot
@@ -44,6 +45,11 @@ public class CharBeforePrologCheckTest extends AbstractCheckTester {
   public void ok_with_bom() throws Exception {
     XmlSourceCode sourceCode = parseAndCheck(UTF8_BOM_FILE, new CharBeforePrologCheck());
     assertEquals(INCORRECT_NUMBER_OF_VIOLATIONS, 0, sourceCode.getXmlIssues().size());    
+  }
+
+  @Override
+  protected DefaultFileSystem createFileSystem() {
+    return super.createFileSystem().setEncoding(StandardCharsets.UTF_8);
   }
 
 }
