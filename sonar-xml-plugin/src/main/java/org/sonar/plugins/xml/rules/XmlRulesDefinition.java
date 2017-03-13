50ee65800d11e46678ac1fa067cc1408205f0923
Yves Dubois-Pelerin <yves.duboispelerin@sonarsource.com>
2017-03-13 17:31:52 +0100

    SONARXML-46 Get the rules definition from the HTML/JSON files instead of the annotations on the check classes

--- sonar-xml-plugin/src/main/java/org/sonar/plugins/xml/rules/XmlRulesDefinition.java
+++ sonar-xml-plugin/src/main/java/org/sonar/plugins/xml/rules/XmlRulesDefinition.java
@@ -19,6 +19,16 @@
  */
 package org.sonar.plugins.xml.rules;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import javax.annotation.Nullable;
+import org.sonar.api.internal.google.gson.Gson;
+import org.sonar.api.rule.RuleStatus;
+import org.sonar.api.rules.RuleType;
+import org.sonar.api.server.debt.DebtRemediationFunction;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.plugins.xml.checks.CheckRepository;
 import org.sonar.plugins.xml.language.Xml;
@@ -29,6 +39,8 @@ import org.sonar.squidbridge.annotations.AnnotationBasedRulesDefinition;
  */
 public final class XmlRulesDefinition implements RulesDefinition {
 
+  private final Gson gson = new Gson();
+
   @Override
   public void define(Context context) {
     NewRepository repository = context
@@ -37,7 +49,73 @@ public final class XmlRulesDefinition implements RulesDefinition {
 
     new AnnotationBasedRulesDefinition(repository, Xml.KEY).addRuleClasses(false, CheckRepository.getCheckClasses());
 
+    for (NewRule rule : repository.rules()) {
+      String metadataKey = rule.key();
+      // Setting internal key is essential for rule templates (see SONAR-6162), and it is not done by AnnotationBasedRulesDefinition from sslr-squid-bridge version 2.5.1:
+      rule.setInternalKey(metadataKey);
+      rule.setHtmlDescription(readRuleDefinitionResource(metadataKey + ".html"));
+      addMetadata(rule, metadataKey);
+    }
+
     repository.done();
+  }
+
+  @Nullable
+  private static String readRuleDefinitionResource(String fileName) {
+    URL resource = XmlRulesDefinition.class.getResource("/org/sonar/l10n/xml/rules/xml/" + fileName);
+    if (resource == null) {
+      return null;
+    }
+    try {
+      return Resources.toString(resource, Charsets.UTF_8);
+    } catch (IOException e) {
+      throw new IllegalStateException("Failed to read " + resource, e);
+    }
+  }
+
+  private void addMetadata(NewRule rule, String metadataKey) {
+    String json = readRuleDefinitionResource(metadataKey + ".json");
+    if (json != null) {
+      RuleMetadata metadata = gson.fromJson(json, RuleMetadata.class);
+      rule.setSeverity(metadata.defaultSeverity.toUpperCase(Locale.US));
+      rule.setName(metadata.title);
+      rule.setTags(metadata.tags);
+      rule.setType(RuleType.valueOf(metadata.type));
+      rule.setStatus(RuleStatus.valueOf(metadata.status.toUpperCase(Locale.US)));
+
+      if (metadata.remediation != null) {
+        // metadata.remediation is null for template rules
+        rule.setDebtRemediationFunction(metadata.remediation.remediationFunction(rule.debtRemediationFunctions()));
+        rule.setGapDescription(metadata.remediation.linearDesc);
+      }
+    }
+  }
+
+  private static class RuleMetadata {
+    String title;
+    String status;
+    String type;
+    @Nullable Remediation remediation;
+    String[] tags;
+    String defaultSeverity;
+  }
+
+  private static class Remediation {
+    String func;
+    String constantCost;
+    String linearDesc;
+    String linearOffset;
+    String linearFactor;
+
+    private DebtRemediationFunction remediationFunction(DebtRemediationFunctions drf) {
+      if (func.startsWith("Constant")) {
+        return drf.constantPerIssue(constantCost.replace("mn", "min"));
+      }
+      if ("Linear".equals(func)) {
+        return drf.linear(linearFactor.replace("mn", "min"));
+      }
+      return drf.linearWithOffset(linearFactor.replace("mn", "min"), linearOffset.replace("mn", "min"));
+    }
   }
 
 }
