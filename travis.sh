717df54b8af9481f948e6bf4b34bcb197b5b1a82
Michael Gumowski <michael.gumowski@sonarsource.com>
2018-11-29 14:47:05 +0100

    Update to latest version of travis-utils

--- travis.sh
+++ travis.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v43 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v52 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
 installTravisTools
-source ~/.local/bin/installMaven35
+. ~/.local/bin/installMaven35
 
 case "$TEST" in
 
