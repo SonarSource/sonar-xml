fef4c4698a264e3898469abcd5056a187fe68ae7
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-02-05 14:07:56 +0100

    SONARXML-94 Change issue message for S1120 (#148)

--- sonar-xml-plugin/src/main/java/org/sonar/plugins/xml/checks/IndentationCheck.java
+++ sonar-xml-plugin/src/main/java/org/sonar/plugins/xml/checks/IndentationCheck.java
@@ -23,6 +23,8 @@ import java.util.Collections;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
+import org.sonar.plugins.xml.Xml;
+import org.sonarsource.analyzer.commons.annotations.DeprecatedRuleKey;
 import org.sonarsource.analyzer.commons.xml.XmlFile;
 import org.sonarsource.analyzer.commons.xml.XmlTextRange;
 import org.sonarsource.analyzer.commons.xml.checks.SonarXmlCheck;
@@ -31,19 +33,15 @@ import org.w3c.dom.Node;
 
 import static org.sonar.plugins.xml.Utils.isSelfClosing;
 
-/**
- * RSPEC-1120
- */
-@Rule(key = IndentationCheck.RULE_KEY)
+@Rule(key = "S1120")
+@DeprecatedRuleKey(ruleKey = "IndentCheck", repositoryKey = Xml.REPOSITORY_KEY)
 public class IndentationCheck extends SonarXmlCheck {
 
-  public static final String RULE_KEY = "IndentCheck";
-
-  private static final String MESSAGE = "Make this element start at column %s.";
+  private static final String MESSAGE = "Make this line start after %d spaces to indent the code consistently.";
 
   @RuleProperty(
     key = "indentSize",
-    description = "Number of white-spaces of an indent. If this property is not set, we just check that the code is indented.",
+    description = "Number of white spaces of an indent. If this property is not set, we just check that the code is indented.",
     defaultValue = "2",
     type = "INTEGER")
   private int indentSize = 2;
@@ -116,7 +114,7 @@ public class IndentationCheck extends SonarXmlCheck {
   }
 
   private void reportIssue(XmlTextRange textRange, int expectedIndent) {
-    reportIssue(textRange, String.format(MESSAGE, expectedIndent + 1), Collections.emptyList());
+    reportIssue(textRange, String.format(MESSAGE, expectedIndent), Collections.emptyList());
   }
 
   private static int depth(Node node) {
@@ -210,5 +208,5 @@ public class IndentationCheck extends SonarXmlCheck {
       && node.getNodeType() == Node.TEXT_NODE
       && !node.getTextContent().trim().isEmpty();
   }
-  
+
 }
