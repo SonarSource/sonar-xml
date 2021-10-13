8c41e2da6b0d999d6b8a66ea61f64641b56d8328
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2021-10-13 14:23:35 +0200

    SONARXML-137 Rule S6358: Allowing application backup is security-sensitive

---
+++ sonar-xml-plugin/src/main/java/org/sonar/plugins/xml/checks/security/android/AndroidApplicationBackupCheck.java
@@ -0,0 +1,51 @@
+/*
+ * SonarQube XML Plugin
+ * Copyright (C) 2010-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.xml.checks.security.android;
+
+import java.util.Collections;
+import javax.xml.xpath.XPathExpression;
+import org.sonar.check.Rule;
+import org.sonarsource.analyzer.commons.xml.XPathBuilder;
+import org.sonarsource.analyzer.commons.xml.XmlFile;
+import org.w3c.dom.Element;
+
+@Rule(key = "S6358")
+public class AndroidApplicationBackupCheck extends AbstractAndroidManifestCheck {
+  private static final String MESSAGE = "Make sure backup of application data is safe here.";
+
+  private final XPathExpression xPathApplicationWithBackup = XPathBuilder
+    .forExpression("/manifest/application" +
+      "[" +
+      " not(@n:allowBackup='false')" +
+      "and" +
+      " not(@n:backupAgent)" +
+      "and" +
+      " not(@n:fullBackupContent and (starts-with(@n:fullBackupContent, '@') or starts-with(@n:fullBackupContent, '$')))" +
+      "]")
+    .withNamespace("n", "http://schemas.android.com/apk/res/android")
+    .build();
+
+  @Override
+  protected void scanAndroidManifest(XmlFile file) {
+    evaluateAsList(xPathApplicationWithBackup, file.getDocument())
+      .forEach(node -> reportIssue(XmlFile.nameLocation((Element) node), MESSAGE, Collections.emptyList()));
+  }
+
+}
