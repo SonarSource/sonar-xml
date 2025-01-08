53cf5dbeef4814985e9d2aa6b65bef1c2c3905e1
Alban Auzeill <alban.auzeill@sonarsource.com>
2025-01-08 14:29:39 +0100

    SONARXML-218 Remove queries to non-persisted metrics

--- sonar-xml-plugin/src/test/java/org/sonar/plugins/xml/LineCounterTest.java
+++ sonar-xml-plugin/src/test/java/org/sonar/plugins/xml/LineCounterTest.java
@@ -16,77 +16,58 @@
  */
 package org.sonar.plugins.xml;
 
+import com.google.gson.Gson;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.Arrays;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
 import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.measures.CoreMetrics;
-import org.sonar.api.measures.FileLinesContext;
-import org.sonar.api.measures.FileLinesContextFactory;
 import org.sonarsource.analyzer.commons.xml.ParseException;
 import org.sonarsource.analyzer.commons.xml.XmlFile;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 class LineCounterTest {
 
-  private FileLinesContextFactory fileLinesContextFactory;
-  private FileLinesContext fileLinesContext;
-
-  @BeforeEach
-  void setUp() {
-    fileLinesContextFactory = mock(FileLinesContextFactory.class);
-    fileLinesContext = mock(FileLinesContext.class);
-    when(fileLinesContextFactory.createFor(any(InputFile.class))).thenReturn(fileLinesContext);
-  }
-
   @Test
   void test_simple_file() throws IOException {
-    verifyMetrics("simple.xml", 1,
-      1, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18);
+    verifyMetrics("simple.xml", 1, """
+        {"ncloc_data":{"1":1,"4":1,"5":1,"6":1,"7":1,"8":1,"9":1,"10":1,"12":1,"13":1,"14":1,"15":1,"16":1,"17":1,"18":1}}""");
   }
 
   @Test
   void test_complex_file() throws IOException {
-    verifyMetrics("complex.xml", 17,
-      1, 2, 3, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 25, 26, 27, 34, 35, 36, 37, 38, 39);
+    verifyMetrics("complex.xml", 17, """
+      {"ncloc_data":{"1":1,"2":1,"3":1,"11":1,"12":1,"13":1,"14":1,"15":1,"16":1,"17":1,"18":1,"19":1,"20":1,"22":1,"25":1,"26":1,"27":1,"34":1,"35":1,"36":1,"37":1,"38":1,"39":1}}""");
   }
 
   @Test
   void test_invalid_file() {
-    assertThrows(ParseException.class, () -> verifyMetrics("invalid.xml", -1));
+    assertThrows(ParseException.class, () -> verifyMetrics("invalid.xml", -1, ""));
   }
 
   @Test // SONARXML-19
   void test_file_with_char_before_prolog() throws Exception {
-    verifyMetrics("char_before_prolog.xml", 1, 3, 6, 7, 8, 9, 10, 11, 12, 13);
+    verifyMetrics("char_before_prolog.xml", 1, """
+      {"ncloc_data":{"3":1,"6":1,"7":1,"8":1,"9":1,"10":1,"11":1,"12":1,"13":1}}""");
   }
 
-  private void verifyMetrics(String filename, int commentLinesNumber, int... linesOfCode) throws IOException {
+  private void verifyMetrics(String filename, int commentLinesNumber, String linesMetrics) throws IOException {
     File moduleBaseDir = new File("src/test/resources/parsers/linecount");
     SensorContextTester context = SensorContextTester.create(moduleBaseDir);
     InputFile inputFile = createInputFile(moduleBaseDir.toPath(), filename);
-    LineCounter.analyse(context, fileLinesContextFactory,  XmlFile.create(inputFile));
-
-    Arrays.stream(linesOfCode).forEach(line ->
-      verify(fileLinesContext).setIntValue(CoreMetrics.NCLOC_DATA_KEY, line, 1));
-
-    verify(fileLinesContext).save();
-    verifyNoMoreInteractions(fileLinesContext);
-
-    assertThat(context.measure(inputFile.key(), CoreMetrics.NCLOC).value()).isEqualTo(linesOfCode.length);
+    var fileLinesContext = new FileLinesContextTester();
+    LineCounter.analyse(context, fileLinesContext, XmlFile.create(inputFile));
+    Map<String, Map<Integer, Object>> metrics = fileLinesContext.metrics(inputFile);
+    assertThat(new Gson().toJson(metrics)).isEqualTo(linesMetrics);
+    int expectedNcloc = metrics.get(CoreMetrics.NCLOC_DATA_KEY).size();
+    assertThat(context.measure(inputFile.key(), CoreMetrics.NCLOC).value()).isEqualTo(expectedNcloc);
     assertThat(context.measure(inputFile.key(), CoreMetrics.COMMENT_LINES).value()).isEqualTo(commentLinesNumber);
   }
 
